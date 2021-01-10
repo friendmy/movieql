b714ccb58d189b55a1b36c62c6b6eab7a5f46d1e
friendmy <friendmytrue@gmail.com>
2021-01-10 22:24:44 +0900

    #9

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,13 @@
+import { getMovies, getById } from "./db";
+
 const resolvers = {
-  Query: {
-    name: () => "nicolas"
-  }
+    Query: {
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id),
+    },
+    Mutation: {
+        addMovie: (_, { name, score }) => {},
+    },
 };
 
 export default resolvers;
